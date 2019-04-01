dab26f49ee51c102b94eda2f97242acca25f065d
Matt Cormier <cormierm@gmail.com>
2019-03-31 20:33:31 -0400

    Adds toggle door route

--- garageController.py
+++ garageController.py
@@ -1,12 +1,31 @@
-from flask import Flask
+import RPi.GPIO as GPIO
+import time
+from flask import Flask, request
 
 app = Flask(__name__)
 
+door1 = 18
+channels = [door1]
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channels, GPIO.OUT)
 
+@app.route("/")
+def home():
+    return "Garage Door Application"
+
+@app.route("/toggle-door")
+def toggleDoor():
+    channel = request.args.get('channel')
+    if channel in channels:
+        triggerGPIO(channel)
+        return 'triggered ' + channel
+    return "failed"
+
+def triggerGPIO(gpioChannel):
+    GPIO.output(gpioChannel, False)
+    time.sleep(1)
+    GPIO.output(gpioChannel, True)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
